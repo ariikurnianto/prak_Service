025a70a4816fd6b792fda380b4a732c8e61c8b64
AriKurnianto <ari25kurnianto@gmail.com>
2020-11-10 14:31:18 +0700

    First Commit (By Ari Kurnianto)

---
+++ app/src/main/java/id/ac/itn/alrm/NotificationHelper.java
@@ -0,0 +1,45 @@
+package id.ac.itn.alrm;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+public class NotificationHelper extends ContextWrapper {
+    private static final String channel1id = "channel1ID";
+    private static final String channel1name = "Channel 1";
+    private NotificationManager mManager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannels();
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void createChannels() {
+        NotificationChannel channel_1 = new NotificationChannel(channel1id, channel1name, NotificationManager.IMPORTANCE_DEFAULT);
+        channel_1.enableVibration(true);
+        channel_1.setLightColor(R.color.colorPrimary);
+        channel_1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        getManager().createNotificationChannel(channel_1);
+    }
+    public NotificationManager getManager() {
+        if (mManager == null){
+            mManager = (NotificationManager)
+                    getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return mManager;
+    }
+    public NotificationCompat.Builder
+    getChannel_1Notification(){
+        return new NotificationCompat.Builder(getApplicationContext(), channel1id).setSmallIcon(R.drawable.ic_launcher_background);
+    }
+}
